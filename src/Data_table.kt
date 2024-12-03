9e9743f81c155bb356a21e3f3372a0f950a22ec7
ShevArt1 <151970693+ShevArt1@users.noreply.github.com>
2024-12-03 15:38:08 +0300

    Lab2-Task2.2) Добавлены методы в класс Data_table

--- src/Data_table.kt
+++ src/Data_table.kt
@@ -1,2 +1,6 @@
 class Data_table(private val array: Array<Array<Any>>) {
+  fun get(row: Int, col: Int) = array[row][col]
+
+  fun getRowCount() = array.size
+  fun getColCount() = if (array.isNotEmpty()) array[0].size else 0
 }
