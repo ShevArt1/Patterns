497bc3b0c985d4d2cc4463df720b8e0a27e4de93
ShevArt1 <151970693+ShevArt1@users.noreply.github.com>
2024-12-10 16:09:29 +0300

    Data_list_short quick fix

--- src/Data_list_short.kt
+++ src/Data_list_short.kt
@@ -2,8 +2,12 @@ class Data_list_short(studentShortArray: List<Student_short>) : Data_list(studen
     override fun getNames() = listOf("№", "ФИО", "Git", "Контакт для связи")
     override fun getData() = Data_table(buildList {
         var count = 1
-        for (index in getSelected()) {
-            val student = array[index] as StudentShort
+//        for (index in getSelected()) {
+//            val student = array[index] as Student_short
+//            add(listOf(count++, student.surnameWithInitials, student.git, student.contact))
+//        }
+        for (obj in array) {
+            val student = obj as Student_short
             add(listOf(count++, student.surnameWithInitials, student.git, student.contact))
         }
     })
